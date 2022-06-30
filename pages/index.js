3113d5bdba328428bdb8e7cf20a487d71ee1066a
Jaures <jauresbeinajmin@gmail.com>
2022-06-30 15:23:19 +0100

    Gestion props

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {Flex,Box,Text,Button} from '@chakra-ui/react';
 
-const Banner = () => (
+const Banner = ({purpose}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    
+    {purpose}
   </Flex>
 
 )
@@ -12,6 +12,9 @@ export default function Home() {
   return (
     <div >
     <h1>Hello World</h1>
+
+    <Banner purpose={'For Sale'} /> 
+    <Banner purpose={'For Sale'} />
 
     </div>  
   )
