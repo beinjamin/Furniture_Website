328c7bb8fccd7788ffaa1135c74746064f8d1690
Jaures <jauresbeinajmin@gmail.com>
2022-06-30 14:54:21 +0100

    Mise en place des importation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import {Flex,Box,Text,Button} from '@chakra-ui/react'
 
 export default function Home() {
   return (
