982ac164fb7931cf1188ad0cacaeb6b15402782c
Jaures <jauresbeinajmin@gmail.com>
2022-06-30 15:17:26 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import {Flex,Box,Text,Button} from '@chakra-ui/react'
+import {Flex,Box,Text,Button} from '@chakra-ui/react';
 
+const Banner = () => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    
+  </Flex>
+
+)
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
+    <h1>Hello World</h1>
 
     </div>  
   )
